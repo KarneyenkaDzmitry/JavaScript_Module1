72095e82f2a0d9c05a4658a2fc7b352d7599c4b9
Dzmitry Karneyenka <dzmitry_karneyenka@epam.com>
2018-06-19 11:05:30 +0300

    replace "for" into "foreach" and minimise code.

--- Task_1.1.js
+++ Task_1.1.js
@@ -1,27 +1,14 @@
 'use strict';
-
 let fs = require('fs');
 let fname = process.argv[2];
-let result = 'START';
-
-console.log('Beginning state of result is ', result);
 
 fs.readFile(fname, 'utf8', function (err, contents) {
-    if (err) { throw err; }
+    if (err) throw err;
     else {
-        const mass = contents.toString().split('\n');
-        console.log('Inside readFile function body. Result is ', result);
-        for (let i = 1; mass.length > i; i = i + 2) {
-            result = result + mass[i] + " ";
-        }
+        let result = 'START';
+        contents.split('\n').forEach((element, index) => {
+            if (index%2!==0) result += element + ' '; 
+        });
         console.log(result + 'END');
     }
 })
-for (let i = 0; i < 10; i++) {
-    setTimeout(function () {
-        console.log('Inside body function \'setTimeout\' 10 Times 1 msec : ', i);
-    }, 1);
-}
-console.log('Result after readFile function and timeout: ', result);
-
-console.log('END OF SCRIPT');
