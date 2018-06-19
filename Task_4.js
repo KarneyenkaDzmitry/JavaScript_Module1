4ae6363111edd1841def993e05cff2adf1ce7fd0
Dzmitry Karneyenka <dzmitry_karneyenka@epam.com>
2018-06-19 10:34:33 +0300

    refactor code: if-else put into function, correct structure of program.

--- Task_4.js
+++ Task_4.js
@@ -1,31 +1,26 @@
 'use strict'
-
 const request = require('request');
 const yargs = require('yargs').argv;
-let country;
-if (typeof(yargs.country)!=='string' ) {
-    country = 'Belarus'
-} else {
-    country = yargs.country;
-}
-let result;
 let options = {
     url: 'http://services.groupkt.com/country/get/all',
     method: 'GET',
     json: true
 }
-
-request(options, function (err, head, body) {
-    if (err) {
-        throw Error('Error during recieve response. ',err);
-    } else {
-        result= body.RestResponse.result;
-        result.forEach(element => {
-            if (element.name===country){
-                console.log('name: ',element.name);
-                console.log('alpha2_code: ',element.alpha2_code);
-                console.log('alpha3_code: ',element.alpha3_code);
-            }
-        });
-    }
-})
+doRequestAndShowInfo(options, yargs.country);
+function doRequestAndShowInfo(options, country){
+    if (typeof (country) !== 'string') country = 'Belarus'
+    request(options, function (err, head, body) {
+        if (err) throw Error('Error during recieve response. ', err);
+        else {
+            body.RestResponse.result.forEach(element => {
+                if (element.name === country) {
+                    console.log('name: ', element.name);
+                    console.log('alpha2_code: ', element.alpha2_code);
+                    console.log('alpha3_code: ', element.alpha3_code);
+                    process.exit(0);
+                }
+            });
+            console.log('Sorry, there isn\'t suitable name of country. Check entered parameter --country.');
+        }
+    })
+}
